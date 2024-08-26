deb68bdc7b3e5b45fb27beff2f282b7ce7b37970
08Uzer <121750611+08Uzair@users.noreply.github.com>
2024-08-26 17:27:19 +0530

    last commit

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ connection;
 app.use(express.json());
 
 app.use("/api/v1/schools", schoolRouter);
-
+app.get("/", (req, res) => {
+  res.send("Hello My Self Uzer Qureshi");
+});
 app.listen(port, () => {
-  console.log(`App Listening on PORT ${port}`); 
+  console.log(`App Listening on PORT ${port}`);
   connection.connect((err) => {
     if (err) throw err;
     console.log("Database is Connected");
