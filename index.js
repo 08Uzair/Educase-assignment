a8e39cfe77f63d0aef4995c04b6adc3f965a77a2
08Uzer <121750611+08Uzair@users.noreply.github.com>
2024-08-26 17:10:11 +0530

    fixed

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from "express";
-import { userRouter } from "./routes/users.js";
 import { schoolRouter } from "./routes/school.js";
 import { connection } from "./db/connection.js";
 const app = express();
@@ -8,11 +7,10 @@ const port = 8800;
 connection;
 app.use(express.json());
 
-app.use("/api/v1/user", userRouter);
 app.use("/api/v1/schools", schoolRouter);
 
 app.listen(port, () => {
-  console.log(`App Listening on PORT ${port}`);
+  console.log(`App Listening on PORT ${port}`); 
   connection.connect((err) => {
     if (err) throw err;
     console.log("Database is Connected");
--- routes/users.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-
-import { addUser, getUsers } from "../controller/user.js";
-
-export const userRouter = express.Router();
-userRouter.post("/addUser", addUser);
-userRouter.get("/listUser", getUsers);
- 
